ddb6c50f52f1e7dd93631adef2e6b64605fe72da
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-09-05 20:34:03 +0530

    formatted code properly

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-// code here
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+    console.log(enteredAddress);
+
+    axios.get(``);
+}
+
+form.addEventListener("submit", searchAddressHandler);
